1051f5edadc7b8b1015517d53db5e489c80e2155
zuice <jeff.allen127@gmail.com>
2020-01-12 11:54:21 -0500

    Add jest stuff to the root

--- packages/web/lib/useInterval.ts
+++ packages/web/lib/useInterval.ts
@@ -11,9 +11,9 @@ export function useInterval(callback: () => void, delay: number) {
     function tick() {
       savedCallback.current();
     }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
+
+    let id = setInterval(tick, delay);
+
+    return () => clearInterval(id);
   }, [delay]);
 }
